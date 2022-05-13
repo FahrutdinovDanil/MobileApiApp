09c62b76a0ad0c3f9a3bc8dc016a7b285e8c60a6
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-05-13 23:00:03 +0300

    add weather icons

--- WeatherApi/WeatherApi/Models/WeatherRoot.cs
+++ WeatherApi/WeatherApi/Models/WeatherRoot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
 namespace WeatherApi.Models
@@ -69,6 +70,18 @@ namespace WeatherApi.Models
         public string Main { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
+        public UriImageSource IconImage
+        {
+            get
+            {
+                return new UriImageSource
+                {
+                    Uri = new Uri($"https://openweathermap.org/img/wn/{Icon}@2x.png", UriKind.Absolute),
+                    CachingEnabled = true,
+                    CacheValidity = TimeSpan.FromDays(1)
+                };
+            }
+        }
     }
 
     public class Wind
